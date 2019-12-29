281b793ac48a7f62f562e9a361841952dc49bbcf
Riddhi Doshi <48765727+rdoshi29@users.noreply.github.com>
2019-12-30 01:45:54 +0530

    Update add.php

--- add.php
+++ add.php
@@ -22,6 +22,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Jura" />
 
 </head>
 <body>
@@ -29,43 +30,47 @@
 <div class="jumbotron" style="height: 180px; width: 100%; margin-top: 10px; padding: 2rem 2rem; opcaity: 0.8;">
     <h2>Task Scheduler</h2>      
     <p>A website that helps you prioritize your tasks.</p>
+    <button type="submit" class="btn btn-secondary" name="addbutton" onclick="location.href = 'index.php';" style="border:0px;margin-left: 25px; margin-top: 0px;"><img src="https://img.icons8.com/flat_round/2x/back--v1.png"></button>
+ 
   <div class="dropdown" style="float: right;">
     <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">
     <img src="https://icon-library.net/images/user-icon-image/user-icon-image-21.jpg">
     </button>
     <div class="dropdown-menu">
-    
-    <br><button type="submit" class="btn btn-danger" name="addbutton" onclick="location.href = 'index.php';" style="border: 0px ;margin-left: 25px; margin-top: 0px; background-color: #ffffff;"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCR4pyd0JtvLYD2XcZuv_bdDJCVjcJCdzVyHRDEcrqQXaj5cgh&s" ></button>
     <p class="dropdown-item"><strong><?php echo $_SESSION['username']; ?></strong></p>
       <a class="dropdown-item" href="index.php?logout='1'">Logout</a>
     </div>
-  </div>
+    </div>
   </div>
 <div class="flex-container">
     <div style="overflow-y: scroll; height: 300px; width: 550px; opacity: 0.9;" class='table-responsive'>
     
-    <form action="index.php" method="post" style="width: 100%;">
-        <div class="input-group">
-          <input type="text" placeholder="TaskName" style="width: 75%; border-top-left-radius: 5px;    border-bottom-left-radius: 5px; height: 50px; margin-left: 15px; margin-right: 15px; margin-top: 5px; margin-bottom: 5px;" id="taskname" class="form-control validate" name="task">
+    <form action="add.php" method="post" style="width: 100%;">
+        <p style="margin-left: 15px; margin-right: 15px; margin-top: 0px; margin-bottom: 0px;">Task name</p>
+      	<div class="input-group">
+          <input type="text" style="width: 75%; border-top-left-radius: 5px;    border-bottom-left-radius: 5px; height: 50px; margin-left: 15px; margin-right: 15px; margin-top: 5px; margin-bottom: 5px;" id="taskname" class="form-control validate" name="task" required>
         </div>
         <p style="margin-left: 15px; margin-right: 15px; margin-top: 0px; margin-bottom: 0px;">Deadline</p>
         <div class="input-group">
           <input type="date" style="width: 75%; border-top-left-radius: 5px;    border-bottom-left-radius: 5px; height: 50px; margin-left: 15px; margin-right: 15px; margin-top: 5px; margin-bottom: 5px;" id="deadline" class="form-control validate" name="deadline" value="<?php echo $deadline; ?>">
         </div>
+	<p style="margin-left: 15px; margin-right: 15px; margin-top: 0px; margin-bottom: 0px;">Description</p>
         <div class="input-group">
-          <textarea style="width: 75%; border-top-left-radius: 5px;    border-bottom-left-radius: 5px; height: 150px; margin-left: 15px; margin-right: 15px; margin-top: 5px; margin-bottom: 5px;" rows="3" placeholder="Description" id="detail" class="form-control validate" name="detail" ></textarea>
+          <textarea style="width: 75%; border-top-left-radius: 5px;    border-bottom-left-radius: 5px; height: 150px; margin-left: 15px; margin-right: 15px; margin-top: 5px; margin-bottom: 5px;" rows="3" id="detail" class="form-control validate" name="detail" ></textarea>
         </div>
-        <div class="input-group">
-        <input type="radio" name="label" value="1">1
-        <input type="radio" name="label" value="2">2
-        <input type="radio" name="label" value="3">3
-        <input type="radio" name="label" value="4">4
+	<div class="input-group">
+  	<select class="custom-select" id="inputGroupSelect01" name="label">
+    		<option selected disabled>Task type</option>
+    		<option value="1">Urgent & Important</option>
+    		<option value="2">Urgent</option>
+    		<option value="3">Important</option>
+	    	<option value="4">Periodic</option>
+  	</select>
         <input type="hidden"  name="taskno">
-        </div>
-      </div>
+	</div>
       <div class="modal-footer" style="border: 0px;">
       <div class="d-flex">
-        <button type="submit" name="insert234" class="btn btn-info" style="opacity: 1;"><img src="https://cdn2.iconfinder.com/data/icons/web-application-icons-part-2/100/Artboard_73-512.png"></button>
+        <button type="submit" name="insert" class="btn btn-info" style="opacity: 1;"><img src="https://cdn2.iconfinder.com/data/icons/web-application-icons-part-2/100/Artboard_73-512.png"></button>
       </div>
       </div>  
 
@@ -78,3 +83,27 @@
 
 </html>
 
+<?php
+  if(isset($_POST["insert"]))
+  {
+     include('connect.php'); 
+     $hey = $_SESSION['username'];
+     $tn = $_POST['task'];
+     $dl = $_POST['deadline'];
+     $det = $_POST['detail'];
+     $l= $_POST['label'];
+     if ($dl == 0000-00-00)
+     {
+       $dl = date('Y/m/d');
+       $dl = date('Y/m/d',strtotime($dl.'+3 days'));
+     }
+     $sql = "INSERT INTO task (`username`, `tname`, `detail`, `label`, `deadline`) VALUES ('$hey', '$tn', '$det', '$l', '$dl')";
+     if ($conn->query($sql) === TRUE) {
+       echo "New record created successfully";
+       } else {
+       echo "Error: " . $sql . "<br>" . $conn->error;
+       }
+       $conn->close(); 
+       header("location: index.php");
+   }
+  ?>
