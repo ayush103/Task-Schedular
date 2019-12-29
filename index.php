21bf0b268a7da8f2fb093e5e99ff7be38793322a
Riddhi Doshi <48765727+rdoshi29@users.noreply.github.com>
2019-12-30 01:46:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -17,11 +17,13 @@
   <title>Task Scheduler</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  
   <link rel="stylesheet" type="text/css" href="style.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Jura" />
 
 </head>
 <body>
@@ -29,24 +31,26 @@
 <div class="jumbotron" style="height: 180px; width: 100%; margin-top: 10px; padding: 2rem 2rem; opcaity: 0.8;">
     <h2>Task Scheduler</h2>      
     <p>A website that helps you prioritize your tasks.</p>
+	<button type="submit" class="btn btn-secondary" name="addbutton" onclick="location.href = 'add.php';" style="border:0px;margin-left: 25px; margin-top: 0px;"><img src="https://img.icons8.com/cotton/2x/plus.png"></button>
+ 
   <div class="dropdown" style="float: right;">
     <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">
     <img src="https://icon-library.net/images/user-icon-image/user-icon-image-21.jpg">
     </button>
     <div class="dropdown-menu">
-    
-    <br><button type="submit" class="btn btn-danger" name="addbutton" onclick="location.href = 'add.php';" style="border:0px;margin-left: 25px; margin-top: 0px; background-color: #ffffff;"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAPFBMVEX///8AAADQ0ND09PQWFhaUlJSxsbF2dnbc3NywsLDCwsJWVla9vb2AgIDn5+cjIyPW1tZkZGRwcHCYmJgU8++KAAABOElEQVR4nO3dUVLCQBBFUWIQEBVR979XXYCOlQqxHzPnLCDV9yM/6VT1bgcAAAAAAAAAAABAltPhF0/nuXq2m3ieGl6qp7uB11bgNF2q51vvrV14qJ5vtXnfLnysHnA1hQrzKVSYT6HCfAoV5lOoMJ9ChfkUKsynUGE+hQrzKVSYT6HCfAoV5lOoMJ9ChfkUflYP+IPLwxLnPwqP1yVPu/7HL7fv7Yk3tt/+l9uP0sBvp60LqwM3f3Hn6sDpqFChwupAhQoVKlSoUKFChQoVKlSoUKFChYMU9v/Ne4C9Rf+7pwH2hwvd4w54GXt8hfkUKsynUGE+hQrzKVSYT6HCfAoV5lOoMJ9ChfkUKsynUGE+hQrzKVSYT6HCfArvv7D/u2sD3M7r//7hADcsAQAAAAAAAAAAADryBfaAJNKTp1f+AAAAAElFTkSuQmCC" ></button>
     <p class="dropdown-item"><strong><?php echo $_SESSION['username']; ?></strong></p>
       <a class="dropdown-item" href="index.php?logout='1'">Logout</a>
     </div>
   </div>
   </div>
 <div class="flex-container">
+
     <div style="border: 1px solid #ffffff; background: #ffb3ba; overflow-y: scroll; height: 300px; width: 550px; opacity: 0.8;" class='table-responsive'>
     <?php
         include('connect.php');
+        #$sql = "SELECT taskid FROM task where username='$hey' order by label ASC, date(deadline) ASC"
         $hey=$_SESSION['username'];
-        $sql = "SELECT * FROM task where username='$hey' and label='1'and deadline > CURRENT_DATE order by date(deadline)";
+        $sql = "SELECT * FROM task where username='$hey' and label='1' and done=0 order by date(deadline)";
         $result = mysqli_query($conn, $sql);
         echo "<br>";
         echo "<table class='table table-hover table-fixed'>
@@ -71,7 +75,7 @@
     <?php
         include('connect.php');
         $hey=$_SESSION['username'];
-        $sql = "SELECT * FROM task where username='$hey' and label='2' and deadline > CURRENT_DATE order by date(deadline)";
+        $sql = "SELECT * FROM task where username='$hey' and label='2' and done=0 order by date(deadline)";
         $result = mysqli_query($conn, $sql);
         echo "<br>";
         echo "<table class='table table-hover table-fixed'>
@@ -95,7 +99,7 @@
     <?php
         include('connect.php');
         $hey=$_SESSION['username'];
-        $sql = "SELECT * FROM task where username='$hey' and label='3' and deadline > CURRENT_DATE order by date(deadline)";
+        $sql = "SELECT * FROM task where username='$hey' and label='3' and done=0 order by date(deadline)";
         $result = mysqli_query($conn, $sql);
         echo "<br>";
         echo "<table class='table table-hover table-fixed'>
@@ -119,7 +123,7 @@
     <?php
         include('connect.php');
         $hey=$_SESSION['username'];
-        $sql = "SELECT * FROM task where username='$hey' and label='4' and deadline > CURRENT_DATE order by date(deadline)";
+        $sql = "SELECT * FROM task where username='$hey' and label='4' and done=0 order by date(deadline)";
         $result = mysqli_query($conn, $sql);
         echo "<br>";
         echo "<table class='table table-hover table-fixed'>
@@ -140,24 +144,26 @@
       ?>
     </div>
 <div style="border: 1px solid #ffffff; background: #ffdfba; width: 1100px; overflow-y: scroll; height: 300px; opacity: 0.8;" class="table-responsive">
-    <p> COMPLETED TASKS </p>
+<p class="lead">
+  Completed tasks 
+</p>
     <?php
         include('connect.php');
         $hey=$_SESSION['username'];
-        $sql = "SELECT * FROM task where username='$hey' and deadline < CURRENT_DATE order by date(deadline) DESC;";
+        $sql = "SELECT * FROM task where username='$hey' and done=1 order by date(deadline) DESC;";
         $result = mysqli_query($conn, $sql);
         echo "<br>";
         echo "<table class='table table-hover table-fixed'>
           <thead>
             <tr>
-              <th scope='col'> #</th>
               <th scope='col'>TASK</th>
+              <th scope='col'>DESCRIPTION</th>
               <th scope='col'>DEADLINE</th>
             </tr>
           </thead><tbody>";
         if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
-              echo"<tr><th scope='row'><button type='button' class='btn btn-default view_data' data-toggle='modal' data-target='#modal1' id=".$row['taskid']."><img src='https://image.flaticon.com/icons/png/512/84/84380.png'></button></th><td>".$row["tname"]."</td><td>".$row["deadline"]."</td></tr>";
+              echo"<tr><td>".$row["tname"]."</td><td>".$row["detail"]."</td><td>".$row["deadline"]."</td></tr>";
             }
         }
         echo "</tbody></table>";
@@ -174,20 +180,6 @@
                 </div>  
                 <div class="modal-body" id="task_detail">
 
-                </div> 
-          </div>             
-      </div>  
-  </div>  
-
-  <div id="modal2" class="modal fade">  
-      <div class="modal-dialog">  
-          <div class="modal-content">  
-                <div class="modal-header">    
-                    <h4 class="modal-title">New task</h4>
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>  
-                </div>  
-                <div class="modal-body" id="task_new">
-                 
                 </div> 
           </div>             
       </div>  
@@ -213,67 +205,4 @@
            });  
       });  
  });
-
- $(document).ready(function(){  
-      $('.add_data').click(function(){  
-           var label = $(this).attr("label");  
-           $.ajax({  
-                url:"add.php",  
-                method:"post",  
-                data:{label:label},  
-                success:function(data){  
-                     $('#task_new').html(data);  
-                     $('#modal2').modal("show");  
-                }  
-           });  
-      });  
- });  
  </script>
-
-<?php
-if (isset($_POST['save'])) {
-    $ti=$_POST["taskno"];
-    $tn=$_POST["task"];
-    $dl=$_POST["deadline"];
-    $td=$_POST["detail"];
-    include('connect.php');
-    $sql = "UPDATE task SET tname='$tn',detail='$td',deadline='$dl' WHERE taskid='$ti'";
-    if ($conn->query($sql) === TRUE) {
-      echo "Updation successfully";
-      } else {
-      echo "Error: " . $sql . "<br>" . $conn->error;
-      }
-      $conn->close();
-  }
-?>
-<?php
-if (isset($_POST['deletebutton'])) {
-$ti=$_POST["taskno"];
-include('connect.php');
-$sql = "Delete from task WHERE taskid='$ti'";
-if ($conn->query($sql) === TRUE) {
-  echo "Deletion successfully";
-  } else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-  }
-  $conn->close();
-}
-?>
-<?php 
- if(isset($_POST["insert"]))
- {
-    include('connect.php'); 
-    $hey = $_SESSION['username'];
-    $tn = $_POST['task'];
-    $dl = $_POST['deadline'];
-    $det = $_POST['detail'];
-    $l= $_POST['label'];
-    $query = "INSERT INTO task (`taskid`, `username`, `tname`, `detail`, `label`, `deadline`) VALUES (NULL, '$tn', 'Task3', '$det', '$l', '$dl')";
-    $results = mysqli_query($conn, $query);
-    if ($conn->query($sql) === TRUE) {
-      echo "New record created successfully";
-      } else {
-      echo "Error: " . $sql . "<br>" . $conn->error;
-      }
-      $conn->close(); }
-?>
